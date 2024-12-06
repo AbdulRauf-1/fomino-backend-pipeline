bdc2f2dd2216efa2c73b97389a07c80342daac2d
Abdul Rauf <sigidevelopers@gmail.com>
2024-12-06 11:39:10 +0500

    node

--- trigger.php
+++ trigger.php
@@ -3,7 +3,7 @@
 $workingDir = '/home/fomino/testingtsh.fomino.ch';
 
 // Set up the correct environment variables for the shell
-$nodeBinPath = '/home/fomino/.nvm/versions/node/v16.20.2/bin/node'; // Path to node binaries
+$nodeBinPath = '/home/fomino/.nvm/versions/node/v16.20.2/bin'; // Path to node binaries
 $pm2BinPath = '/home/fomino/bin/pm2'; // Path to PM2 binary directory
 
 // Set the PATH environment variable explicitly using putenv()
