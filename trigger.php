aade9d30acdc4a7feb3fba7f83df95d220127b16
Abdul Rauf <sigidevelopers@gmail.com>
2024-12-06 12:25:06 +0500

    node

--- trigger.php
+++ trigger.php
@@ -9,6 +9,12 @@ $nvmPath = '/home/fomino/.nvm/nvm.sh'; // The path to nvm's script
 $nodeBinPath = '/home/fomino/.nvm/versions/node/v16.20.2/bin'; // Path to node binaries directory
 $npmPath = '/home/fomino/.nvm/versions/node/v16.20.2/bin/npm';
 
+// Set permissions for node and npm binaries
+$setBinaryPermissions = shell_exec("chmod +x $nodeBinPath $npmPath 2>&1");
+
+// Set permissions for the working directory
+$setDirPermissions = shell_exec("chmod -R 775 $workingDir 2>&1");
+
 // Ensure NVM is loaded and the correct paths are set
 putenv("NVM_DIR=/home/fomino/.nvm");
 $command = "source $nvmPath && nvm use 16.20.2 && export PATH=$nodeBinPath:$npmPath:" . getenv('PATH') . " && export HOME=/home/fomino && cd $workingDir && npm install 2>&1";
