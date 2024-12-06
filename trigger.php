47dfc8a2137d8fc75db4eb4f41eeb014fdbac898
Abdul Rauf <sigidevelopers@gmail.com>
2024-12-06 12:07:25 +0500

    node

--- trigger.php
+++ trigger.php
@@ -5,15 +5,16 @@ $workingDir = '/home/fomino/testingtsh.fomino.ch';
 // Set up the correct environment variables for the shell
 $nodeBinPath = '/home/fomino/.nvm/versions/node/v16.20.2/bin/node'; // Path to node binaries
 $npmPath = '/home/fomino/.nvm/versions/node/v16.20.2/bin/npm';
+$Paths = '/home/fomino/.nvm/versions/node/v16.20.2/bin/';
 
 // Set the PATH environment variable explicitly using putenv()
-putenv("PATH=$npmPath:" . getenv('PATH')); // Prepend nodeBinPath to system PATH
+putenv("PATH=$npmBinPath:$npmPath:$paths" . getenv('PATH')); // Prepend nodeBinPath to system PATH
 
 // Command to create the PM2 process and save it again
-$pm2CreateCommand = "npm install";// && pm2 start app.js --name $processName && pm2 save";
+$npmCommand = "npm install";
 
 // Combine all commands
-$command = "export HOME=/home/fomino && cd $workingDir && $pm2CreateCommand 2>&1";
+$command = "export HOME=/home/fomino && cd $workingDir && $npmCommand 2>&1";
 // Execute the command
 $output = shell_exec($command);
 
