9f8725386bf7d3536d02f8df296349ae9635c8d4
Abdul Rauf <sigidevelopers@gmail.com>
2024-12-06 11:45:07 +0500

    node

--- trigger.php
+++ trigger.php
@@ -3,11 +3,12 @@
 $workingDir = '/home/fomino/testingtsh.fomino.ch';
 
 // Set up the correct environment variables for the shell
-$nodeBinPath = '/home/fomino/.nvm/versions/node/v16.20.2/bin'; // Path to node binaries
+$nodeBinPath = '/home/fomino/.nvm/versions/node/v16.20.2/bin/node'; // Path to node binaries
+$npmPath = '/home/fomino/.nvm/versions/node/v16.20.2/bin/npm';
 $pm2BinPath = '/home/fomino/bin/pm2'; // Path to PM2 binary directory
 
 // Set the PATH environment variable explicitly using putenv()
-putenv("PATH=$nodeBinPath:" . getenv('PATH')); // Prepend nodeBinPath to system PATH
+putenv("PATH=$nodeBinPath:$npmPath:" . getenv('PATH')); // Prepend nodeBinPath to system PATH
 
 // Define the process name for clarity
 $processName = 'rauf';
